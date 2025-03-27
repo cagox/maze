6bb928d120b9853d6c3cc0e64b14b1bc99e0ee88
Cagox <cagox@cagox.net>
2025-03-27 22:09:35 +0900

    Initial Push.
    I am working on the maze generation project on boot.dev.
    This is a very early iteration of the code.

---
+++ src/app.py
@@ -0,0 +1,37 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width=420, height=420):
+        conf={
+            'bg': "white",
+            'height': f"{height}",
+            'width': f"{width}",
+            }
+        self.__root = Tk()
+        #self.__root.minsize(width, height)
+        self.__root.geometry(f"{width}x{height}")
+        self.__root.title("Maze Solver")
+        self.canvas = Canvas(self.__root, conf)
+        self.canvas.pack()
+        self.running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+    
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+    
+    def close(self):
+        self.running = False
+
+
+
+
+
+
+
---
+++ src/main.py
@@ -0,0 +1,5 @@
+from app import Window
+
+win = Window(800, 600)
+win.wait_for_close()
+
